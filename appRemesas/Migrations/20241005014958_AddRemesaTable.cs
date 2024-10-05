9b2146dd2a99d0d716189644884578280058922e
Michael <michaelorihuela00@gmail.com>
2024-10-04 21:45:49 -0500

    Funcionalidad registro y listar

---
+++ appRemesas/Migrations/20241005014958_AddRemesaTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace appRemesas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemesaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "remesas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Remitente = table.Column<string>(type: "text", nullable: false),
+                    Destinatario = table.Column<string>(type: "text", nullable: false),
+                    PaisOrigen = table.Column<string>(type: "text", nullable: false),
+                    PaisDestino = table.Column<string>(type: "text", nullable: false),
+                    Monto = table.Column<decimal>(type: "numeric", nullable: false),
+                    Moneda = table.Column<string>(type: "text", nullable: false),
+                    TasaCambio = table.Column<decimal>(type: "numeric", nullable: false),
+                    MontoFinal = table.Column<decimal>(type: "numeric", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_remesas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "remesas");
+        }
+    }
+}
--- appRemesas/obj/Debug/net8.0/appRemesas.AssemblyInfo.cs
+++ appRemesas/obj/Debug/net8.0/appRemesas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appRemesas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd02fc0f1be767f427348a394e7c86dd29b4efcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("appRemesas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appRemesas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
